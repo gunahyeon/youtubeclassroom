b97a6da76a961285013c2f5dd788165a583559da
gunahyeon <rnskqkd@naver.com>
2022-06-16 02:30:56 +0900

    비밀정보 보호

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI : process.env.MONGO_URI
+}
+//Heroku 사이트에서 들어온 요청
--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const port = 5001;
 const mongoose = require("mongoose");
 const { User } = require("./models/Users");
 const bodyParser = require("body-parser");
-
+const config = require('./config/key');
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended:true}));
 //application/json
 app.use(bodyParser.json());
 
 
-mongoose.connect('mongodb+srv://gunahyeon:1111@lecture-project.cnj0j7y.mongodb.net/?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true,
 }).then(()=>console.log("MongoDB connected..."))
   .catch(err => console.log(err));
