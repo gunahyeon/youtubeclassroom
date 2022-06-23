bda84793290a1d9965d6b92b0c716d438c7893a7
gunahyeon <rnskqkd@naver.com>
2022-06-23 10:31:35 +0900

    회원가입, 로그인/로그아웃 구현

--- index.js
+++ index.js
@@ -69,8 +69,19 @@ app.get('/api/users/auth', auth,(req,res)=>{
         name:req.user.name, 
         lastname:req.user.lastname,
         role:req.user.role,
-        image:req.user.image})
+        image:req.user.image
+    })
 })
 
+app.get('/api/users/logout', auth, (req,res)=>{
+    User.findOneAndUpdate({_id:req.user._id}, 
+        {token : ""},
+        (err,user)=>{
+            if(err) return res.json({success:false, err});
+            return res.status(200).send({
+                success:true
+            })
+        })
+})
 
 app.listen(port, () => console.log(`example app listening on port ${port}`));
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,18 @@
-const { User } = require("../models/Users");
+const {User}  = require("../models/Users");
 
-let auth = (req, res, next) => {
+var auth = (req, res, next) => {
     //인증 처리를 하는 곳
     //get token from client cookie
-    let token = req.cookies.x_auth;
+    var token = req.cookies.x_auth;
     //decode token and find user
-    User.fineByToken(token, (err,user)=>{
+    User.findByToken(token, (err,user) => {
         if(err) throw err;
         if(!user) return res.json({isAuth:false, error:true});
+        
+        req.token = token;
+        req.user = user;
+        next();
     })
-    req.token = token;
-    req.user = user;
-    next();
     //if user, auth ok
     //if !user, auth fail
 }
